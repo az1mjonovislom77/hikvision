faf0ede9a98aeb1ccc549c8880b70ec8edf917c8
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2025-12-17 14:22:18 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3002",
     "http://localhost:3003",
     "http://localhost:3004",
-    "https://45.55.129.34/"
+    "https://45.55.129.34"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
