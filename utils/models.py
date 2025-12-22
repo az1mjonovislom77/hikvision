e794790ac43c3981c82bc990010d45f606c09f2e
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2025-12-22 15:57:23 +0500

    done

--- utils/models.py
+++ utils/models.py
@@ -50,7 +50,7 @@ class TelegramChannel(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=200)
     chat_id = models.CharField(max_length=200)
-    resolve_id = models.CharField(max_length=200, blank=True, null=True)
+    resolved_id = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.name
