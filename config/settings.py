68307acae972ad21fb69fffd01cd9836549fed72
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2025-12-25 15:40:53 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 ENVIRON = config('ENVIRON', default='local')
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
