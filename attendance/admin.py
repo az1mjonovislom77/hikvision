b2221dcbf4038453b13650426ff66106f3790aca
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-01-06 16:11:34 +0500

    done

--- attendance/admin.py
+++ attendance/admin.py
@@ -4,6 +4,6 @@ from .models import AttendanceDaily
 
 @admin.register(AttendanceDaily)
 class AttendanceDailyAdmin(admin.ModelAdmin):
-    list_display = ("employee", "date", "status", "comment")
+    list_display = ("employee__name", "date", "status", "comment")
     list_filter = ("status", "date", "employee")
     search_fields = ("employee__name",)
