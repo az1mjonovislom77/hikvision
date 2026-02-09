36e5063315eb179f00ec8a01fc9d28563d8078a8
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-09 09:54:17 +0500

    finished

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
