92e33f2a71d2bbb40200c4a67beeac461f39eb74
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-04-14 09:50:41 +0500

    done

--- utils/telegram/telegram.py
+++ utils/telegram/telegram.py
@@ -1,7 +1,10 @@
 import logging
 import requests
+import socket
 from io import BytesIO
 from decouple import config
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 from requests.auth import HTTPDigestAuth
 
 logger = logging.getLogger(__name__)
@@ -10,28 +13,60 @@ BOT_TOKEN = config("BOT_TOKEN")
 BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
 
+def force_ipv4():
+    return socket.AF_INET
+
+
+requests.packages.urllib3.util.connection.allowed_gai_family = force_ipv4
+
+session = requests.Session()
+
+retries = Retry(total=5, backoff_factor=1, status_forcelist=[500, 502, 503, 504])
+
+adapter = HTTPAdapter(max_retries=retries)
+session.mount("https://", adapter)
+session.mount("http://", adapter)
+
+
 def download_image(url, device):
     try:
-        r = requests.get(url, auth=HTTPDigestAuth(device.username, device.password), timeout=15)
+        r = session.get(url, auth=HTTPDigestAuth(device.username, device.password), timeout=20)
         if r.status_code == 200 and r.content:
             return r.content
-    except Exception:
+    except requests.exceptions.RequestException:
         logger.exception("DOWNLOAD IMAGE ERROR")
+
     return None
 
 
 def send_telegram(chat_id, text, image_bytes=None):
-    if image_bytes:
-        file_obj = BytesIO(image_bytes)
-        file_obj.name = "event.jpg"
+    try:
+        if image_bytes:
+            file_obj = BytesIO(image_bytes)
+            file_obj.name = "event.jpg"
 
-        r = requests.post(f"{BASE_URL}/sendPhoto",
-                          data={"chat_id": chat_id, "caption": text, "parse_mode": "HTML"},
-                          files={"photo": file_obj}, timeout=15)
-    else:
-        r = requests.post(f"{BASE_URL}/sendMessage",
-                          json={"chat_id": chat_id, "text": text, "parse_mode": "HTML", },
-                          timeout=10)
+            r = session.post(
+                f"{BASE_URL}/sendPhoto",
+                data={
+                    "chat_id": chat_id,
+                    "caption": text,
+                    "parse_mode": "HTML"
+                },
+                files={"photo": file_obj},
+                timeout=20
+            )
+        else:
+            r = session.post(
+                f"{BASE_URL}/sendMessage",
+                json={
+                    "chat_id": chat_id,
+                    "text": text,
+                    "parse_mode": "HTML"
+                },
+                timeout=20
+            )
 
-    logger.info(f"Telegram response: {r.text}")
-    r.raise_for_status()
+        logger.info(f"Telegram response: {r.text}")
+
+    except requests.exceptions.RequestException:
+        logger.exception("TELEGRAM SEND ERROR")
--- utils/telegram/telegram_updates.py
+++ utils/telegram/telegram_updates.py
@@ -1,18 +1,22 @@
 import logging
-import requests
 from decouple import config
 from utils.models import TelegramChannel
+from utils.telegram.telegram import session, BASE_URL
 
 logger = logging.getLogger(__name__)
 
 BOT_TOKEN = config("BOT_TOKEN")
-BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
 
 def sync_channels_from_updates():
-    r = requests.get(f"{BASE_URL}/getUpdates", timeout=10)
-    r.raise_for_status()
-    data = r.json()
+    try:
+        r = session.get(f"{BASE_URL}/getUpdates", timeout=20)
+        r.raise_for_status()
+        data = r.json()
+
+    except Exception:
+        logger.exception("TELEGRAM GET UPDATES ERROR")
+        return
 
     for update in data.get("result", []):
         message = update.get("channel_post")
@@ -22,9 +26,13 @@ def sync_channels_from_updates():
         chat = message.get("chat")
         chat_id = str(chat["id"])
         title = chat.get("title")
+
         logger.info(f"📡 Update from channel: {title} ({chat_id})")
 
-        channel = TelegramChannel.objects.filter(name=title, resolved_id__isnull=True).first()
+        channel = TelegramChannel.objects.filter(
+            name=title,
+            resolved_id__isnull=True
+        ).first()
 
         if channel:
             channel.resolved_id = chat_id
