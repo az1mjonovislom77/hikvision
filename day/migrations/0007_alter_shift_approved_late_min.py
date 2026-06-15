4c944aa869dede5b34baa2ee7838433d05f25831
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-06-15 15:27:43 +0500

    done

--- day/migrations/0007_alter_shift_approved_late_min.py
+++ day/migrations/0007_alter_shift_approved_late_min.py
@@ -14,17 +14,27 @@ class Migration(migrations.Migration):
             database_operations=[
                 migrations.RunSQL(
                     sql="""
-                        ALTER TABLE "day_shift"
-                        ALTER COLUMN "approved_late_min" TYPE integer
-                        USING CASE
-                            WHEN "approved_late_min" IS NULL THEN 15
-                            ELSE (
-                                EXTRACT(HOUR FROM "approved_late_min")::integer * 60 +
-                                EXTRACT(MINUTE FROM "approved_late_min")::integer
-                            )
-                        END;
-                        ALTER TABLE "day_shift" ALTER COLUMN "approved_late_min" SET NOT NULL;
-                        ALTER TABLE "day_shift" ALTER COLUMN "approved_late_min" SET DEFAULT 15;
+                        DO $$
+                        BEGIN
+                            IF (
+                                SELECT data_type
+                                FROM information_schema.columns
+                                WHERE table_name = 'day_shift'
+                                  AND column_name = 'approved_late_min'
+                            ) = 'time without time zone' THEN
+                                ALTER TABLE "day_shift"
+                                ALTER COLUMN "approved_late_min" TYPE integer
+                                USING CASE
+                                    WHEN "approved_late_min" IS NULL THEN 15
+                                    ELSE (
+                                        EXTRACT(HOUR FROM "approved_late_min")::integer * 60 +
+                                        EXTRACT(MINUTE FROM "approved_late_min")::integer
+                                    )
+                                END;
+                                ALTER TABLE "day_shift" ALTER COLUMN "approved_late_min" SET NOT NULL;
+                                ALTER TABLE "day_shift" ALTER COLUMN "approved_late_min" SET DEFAULT 15;
+                            END IF;
+                        END $$;
                     """,
                     reverse_sql=migrations.RunSQL.noop,
                 ),
