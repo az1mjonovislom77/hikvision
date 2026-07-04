275708af3c55854ed1c7a39ef33a9f6985ae4617
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-07-04 08:33:28 +0500

    fixed

--- user/models.py
+++ user/models.py
@@ -8,7 +8,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, phone_number, password=None, **extra_fields):
         if not phone_number:
-            raise ValueError("Phone number is required")
+            raise ValueError("Phone number is required!")
         user = self.model(phone_number=phone_number, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
